d5fa72347184ef79da8cce94c5dcc934f3f14af5
carlosM99-MAX <cmgomezcotes@uniguajira.edu.co>
2020-09-17 01:23:04 -0500

    metodo interes por cgomez

---
+++ interes.py
@@ -0,0 +1,15 @@
+def im_interes(self):
+    self.importe = (self.cantidad*self.interes)/100
+    return self.importe
+
+    
+def main():
+
+   
+    caja1 =caja_ahorro()
+    caja1.mostrar_caja()
+
+
+    
+
+if __name__ == "__main__":
